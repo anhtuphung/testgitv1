dd0ed599c96745b5f099ec4d3080c88c5002f632
anhtu <tu23022004@gmail.com>
2025-03-13 19:56:01 +0700

    Create new branch s1

---
+++ src/people.java
@@ -0,0 +1,9 @@
+public class people {
+    static int a = 5;
+    static int b = 5;
+    static int c = 5;
+static int d = a+b+c;
+    public static void main(String[] args) {
+        System.out.printf("anwser = "+ d);
+    }
+}
